b75da0cf3a3a637f2d160c9955c53e9f21c611a6
Faiber-Juanias <faiberjuanias001@gmail.com>
2018-09-06 15:29:22 -0500

    Cree el adaptador para categorias

---
+++ app/src/main/java/sena/prueba/Adaptadores/AdapterCategoria.java
@@ -0,0 +1,73 @@
+package sena.prueba.Adaptadores;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import sena.prueba.R;
+
+public class AdapterCategoria extends RecyclerView.Adapter<AdapterCategoria.AdapterHolderCategoria>{
+
+    //Creo los objetos
+    private Context context;
+    private List<Categoria> categoriasList;
+    private View.OnClickListener listener;
+
+    public AdapterCategoria(Context context, List<Categoria> categoriasList) {
+        this.context = context;
+        this.categoriasList = categoriasList;
+    }
+
+    @NonNull
+    @Override
+    public AdapterCategoria.AdapterHolderCategoria onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View vista = LayoutInflater.from(context).inflate(R.layout.item_list_categoria, parent, false);
+
+        return new AdapterHolderCategoria(vista);
+    }
+
+    public void setOnClickListener(View.OnClickListener listener){
+        this.listener = listener;
+    }
+
+    public void onClick(View view){
+        if (listener != null){
+            listener.onClick(view);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterCategoria.AdapterHolderCategoria holder, int position) {
+        holder.imagenPrincipal.setImageResource(categoriasList.get(position).getImagePrincipal());
+        holder.iconoPrincipal.setImageResource(categoriasList.get(position).getIconoPrincipal());
+        holder.nombreCategoria.setText(categoriasList.get(position).getNombreCategoria());
+    }
+
+    @Override
+    public int getItemCount() {
+        return categoriasList.size();
+    }
+
+    public class AdapterHolderCategoria extends RecyclerView.ViewHolder {
+
+        //Creo los objetos
+        ImageView imagenPrincipal, iconoPrincipal;
+        TextView nombreCategoria;
+
+        public AdapterHolderCategoria(View itemView) {
+            super(itemView);
+
+            //Creo las referencias
+            imagenPrincipal = (ImageView) itemView.findViewById(R.id.image_principal);
+            iconoPrincipal = (ImageView) itemView.findViewById(R.id.icono_principal);
+            nombreCategoria = (TextView) itemView.findViewById(R.id.nombre_categoria_principal);
+        }
+    }
+}
--- app/src/main/java/sena/prueba/MainActivity.java
+++ app/src/main/java/sena/prueba/MainActivity.java
@@ -1,39 +1,75 @@
 package sena.prueba;
 
-import android.content.Intent;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Surface;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import sena.prueba.Adaptadores.AdapterCategoria;
+import sena.prueba.Adaptadores.Categoria;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView visitar,dormir,comer,hacer,servicios,llamada;
+
+    private RecyclerView recyclerCategorias;
+    private List<Categoria> categoriaList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
-        visitar=findViewById(R.id.visitaImagen);
-        dormir=findViewById(R.id.dormirImagen);
-        comer=findViewById(R.id.comerImagen);
-        hacer=findViewById(R.id.hacerImagen);
-        servicios=findViewById(R.id.servicioImagen);
-        llamada=findViewById(R.id.llamadaImagen);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Creamos las referencias
+        recyclerCategorias = (RecyclerView) findViewById(R.id.recycler_principal);
 
-    }
-
-    public void onClick(View view){
-        switch (view.getId()){
-            case R.id.visitaImagen:
-                startActivity(new Intent(MainActivity.this,list_item_categoria.class));
+        if (getRotation().equals("Horizontal") || getRotation().equals("Horizontal_inversa")){
+            recyclerCategorias.setLayoutManager(new GridLayoutManager(this, 2));
         }
+
+        recyclerCategorias.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        categoriaList = new ArrayList<>();
+        categoriaList.add(new Categoria(R.drawable.ic_launcher_background, R.drawable.ic_launcher_background, "Este es mi titulo"));
+
+        //Creamos una instancia del adaptador de Categorias
+        AdapterCategoria adapter = new AdapterCategoria(this, categoriaList);
+        recyclerCategorias.setAdapter(adapter);
+
+        recyclerCategorias.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Tocaste un item de categoria.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-
-
-
+    public String getRotation() {
+        final int rotation = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getRotation();
+        String mensaje = null;
+        switch (rotation){
+            case Surface.ROTATION_0:
+                mensaje = "Vertical";
+                break;
+            case Surface.ROTATION_90:
+                mensaje = "Horizontal";
+                break;
+            case Surface.ROTATION_180:
+                mensaje = "Vertical_inversa";
+                break;
+            case Surface.ROTATION_270:
+                mensaje = "Horizontal_inversa";
+                break;
+        }
+        return mensaje;
+    }
 }
