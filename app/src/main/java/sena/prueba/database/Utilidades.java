7054ffbc821e37239c1330f382db39c8cf2ac558
Faiber-Juanias <faiberjuanias001@gmail.com>
2018-09-07 13:11:45 -0500

    Se creo el adaptador

---
+++ app/src/main/java/sena/prueba/database/Utilidades.java
@@ -0,0 +1,18 @@
+package sena.prueba.database;
+
+public class Utilidades {
+    //Campos para la tabla Categorias
+    public static final String TBL_CATEGORIA = "Categoria";
+    public static final String ID_TBL_CATEGORIA = "Categoria";
+    public static final String IMAGEN_TBL_CATEGORIA = "Categoria";
+    public static final String ICONO_TBL_CATEGORIA = "Categoria";
+    public static final String NOMBRE_TBL_CATEGORIA = "Categoria";
+    public static final String CREA_TBL_CATEGORIA = "CREATE TABLE " + TBL_CATEGORIA + " (" +
+            ID_TBL_CATEGORIA + " INTEGER PRIMARY KEY, " +
+            IMAGEN_TBL_CATEGORIA + " INTEGER, " +
+            ICONO_TBL_CATEGORIA + " INTEGER, " +
+            NOMBRE_TBL_CATEGORIA + " TEXT)";
+
+
+    //Campos para la tabla
+}
