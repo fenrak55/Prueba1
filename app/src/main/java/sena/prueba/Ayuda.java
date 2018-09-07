e0bb33bbbdd011fd2586b48392ebb33615209b1e
fenrak55 <fenrak55@hotmail.es>
2018-09-07 16:50:05 -0500

    Eventos de Overflow Menu Añadidos

--- app/src/main/java/sena/prueba/Ayuda.java
+++ app/src/main/java/sena/prueba/Ayuda.java
@@ -1,9 +1,12 @@
 package sena.prueba;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
+import java.nio.file.FileSystemNotFoundException;
 
 
 public class Ayuda extends AppCompatActivity {
@@ -21,5 +24,29 @@ public class Ayuda extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_informacion_item,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    //Metodo para pulsar el overflow menu
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_registrarse:
+                Intent intent;
+                startActivity(intent= new Intent(Ayuda.this,Registrarse.class));
+                break;
+            case R.id.menu_salir:
+                /*android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);*/
+                Intent intent1=new Intent(Intent.ACTION_MAIN);
+                intent1.addCategory(Intent.CATEGORY_HOME);
+                intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent1);
+
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/sena/prueba/Registrarse.java
+++ app/src/main/java/sena/prueba/Registrarse.java
@@ -1,15 +1,43 @@
 package sena.prueba;
 
+import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Registrarse extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registrarse);
-        String title= "Registro";
+        setContentView(R.layout.activity_registro);
+        String title= "Registrarse";
         setTitle(title);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_informacion_item,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_ayuda:
+                Intent intent;
+                startActivity(intent= new Intent(Registrarse.this,Ayuda.class));
+                break;
+            case R.id.menu_salir:
+                Intent intent1=new Intent(Intent.ACTION_MAIN);
+                intent1.addCategory(Intent.CATEGORY_HOME);
+                intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent1);
+                /*android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);*/
+                break;
+            }
+        return super.onOptionsItemSelected(item);
     }
 }
