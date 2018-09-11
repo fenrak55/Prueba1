fabf950fc05e23b4604adcfa26b7be16b385349d
stefanny <jsalvarado405misena.edu.co>
2018-09-11 07:57:42 -0600

    Termine el menu de informacion item

--- app/src/main/java/sena/prueba/Registrarse.java
+++ app/src/main/java/sena/prueba/Registrarse.java
@@ -2,19 +2,38 @@ package sena.prueba;
 
 import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class Registrarse extends AppCompatActivity {
+
+    private EditText campoNombre, campoEmail, campoCelular;
+    private Toolbar toolbarRegistro;
+    private ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
-        String title= "Registrarse";
-        setTitle(title);
+
+        //Creamos las referencias
+        campoNombre = (EditText) findViewById(R.id.txt_nombre);
+        campoEmail = (EditText) findViewById(R.id.txt_email);
+        campoCelular = (EditText) findViewById(R.id.txt_celular);
+        toolbarRegistro = (Toolbar) findViewById(R.id.toolbar_registro);
+
+        toolbarRegistro.setTitle("Registro");
+        setSupportActionBar(toolbarRegistro);
+
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -24,18 +43,18 @@ public class Registrarse extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        int id = item.getItemId();
+        Intent intent = null;
+        switch (id){
             case R.id.menu_ayuda:
-                Intent intent;
-                startActivity(intent= new Intent(Registrarse.this,Ayuda.class));
+                intent = new Intent(Registrarse.this, Ayuda.class);
+                startActivity(intent);
                 break;
             case R.id.menu_salir:
-                Intent intent1=new Intent(Intent.ACTION_MAIN);
-                intent1.addCategory(Intent.CATEGORY_HOME);
-                intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent1);
-                /*android.os.Process.killProcess(android.os.Process.myPid());
-                System.exit(1);*/
+                intent=new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
                 break;
             }
         return super.onOptionsItemSelected(item);
